67d94a1ced12354e4aeb165eb1dbc0f03ac72405
unknown <jimgrace@gmail.com>
2015-10-02 12:07:56 -0400

    Restore debug statements.

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataapproval/DataApprovalPermissionsEvaluator.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataapproval/DataApprovalPermissionsEvaluator.java
@@ -203,9 +203,9 @@ class DataApprovalPermissionsEvaluator
                 ( ( userLevel >= dataLevel || mayViewLowerLevelUnapprovedData )
                     && organisationUnitService.isInUserHierarchy( da.getOrganisationUnit() ) );
 
-        log.error("Org unit " + da.getOrganisationUnit().getName() + " in hierarchy: " + organisationUnitService.isInUserHierarchy( da.getOrganisationUnit() ) );
+        log.debug( "Org unit " + da.getOrganisationUnit().getName() + " in hierarchy: " + organisationUnitService.isInUserHierarchy( da.getOrganisationUnit() ) );
 
-        log.error( "getPermissions orgUnit " + ( da.getOrganisationUnit() == null ? "(null)" : da.getOrganisationUnit().getName() )
+        log.debug( "getPermissions orgUnit " + ( da.getOrganisationUnit() == null ? "(null)" : da.getOrganisationUnit().getName() )
             + " workflow " + workflow.getName()
             + " combo " + da.getAttributeOptionCombo().getName() + " state " + s.name()
             + " isApproved " + s.isApproved() + " isApprovable " + s.isApprovable() + " isUnapprovable " + s.isUnapprovable()
