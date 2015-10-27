f176cc20dcd6a3df4fd5813284efa9d640a93d14
Lars Helge Overland <larshelge@gmail.com>
2015-10-26 23:18:11 -0400

    LDAP

---
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -0,0 +1,71 @@
+package org.hisp.dhis.external.conf;
+
+/*
+ * Copyright (c) 2004-2015, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @author Lars Helge Overland
+ */
+public enum ConfigurationKey
+{
+    KEY_LDAP_URL( "ldap.url" ),
+    KEY_LDAP_MANAGER_USERDN( "ldap.manager.userdn" ),
+    KEY_LDAP_MANAGER_PASSWORD( "ldap.manager.password" ),
+    KEY_LDAP_USERDN_PATTERNS( "ldap.userdn.patterns" ),    
+    KEY_FILESTORE_PROVIDER( "filestore.provider", "filesystem" ),
+    KEY_FILE_STORE_CONTAINER( "filestore.container" ),
+    KEY_FILE_STORE_LOCATION( "filestore.location" ),
+    KEY_FILE_STORE_IDENTITY( "filestore.identity" ),
+    KEY_FILE_STORE_SECRET( "filestore.secret" );
+
+    private final String key;
+    
+    private final String defaultValue;
+    
+    ConfigurationKey( String key )
+    {
+        this.key = key;
+        this.defaultValue = null;
+    }
+
+    ConfigurationKey( String key, String defaultValue )
+    {
+        this.key = key;
+        this.defaultValue = defaultValue;
+    }
+    
+    public String getKey()
+    {
+        return key;
+    }
+    
+    public String getDefaultValue()
+    {
+        return defaultValue;
+    }
+}
