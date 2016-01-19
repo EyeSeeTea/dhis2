7c69c13dab9cef2164240988c6d8c1dce1909d92
Yrjan Fraschetti <yrjanaff@gmail.com>
2016-01-19 11:45:42 +0100

    Added folder

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/datastatistics/DataStatistics.java
@@ -0,0 +1,10 @@
+package org.hisp.dhis.datastatistics;
+
+import org.hisp.dhis.common.BaseIdentifiableObject;
+
+/**
+ * Created by yrjanaff on 19.01.2016.
+ */
+public class DataStatistics extends BaseIdentifiableObject {
+
+}
