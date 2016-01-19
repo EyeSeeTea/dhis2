f0742dc878d15a356cabe4f3b86ce86ead89805a
Yrjan Fraschetti <yrjanaff@gmail.com>
2016-01-19 16:26:08 +0100

    Working hello world get request. Pair programming

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datastatistics/DataStatisticsController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datastatistics/DataStatisticsController.java
@@ -1,12 +1,24 @@
-package main.java.org.hisp.dhis.webapi.controller.datastatistics;
+package org.hisp.dhis.webapi.controller.datastatistics;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 /**
  * Created by yrjanaff on 19.01.2016.
  */
 
 @Controller
-//@RequestMapping( value = )
+//@RequestMapping( value = "/datastatistics", method = RequestMethod.GET )
 public class DataStatisticsController
 {
 
+    @RequestMapping(value = "/datastatistics", method = RequestMethod.GET)
+    public @ResponseBody String helloworld(){
+        return "Hello World! Det funka!";
+    }
 }
