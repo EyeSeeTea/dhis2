2b711bf869dfbeeb6b722c3cae57b12b8b83f546
Yrjan Fraschetti <yrjanaff@gmail.com>
2016-01-19 14:17:12 +0100

    Added controller file and implementet interface methods. Pair programming

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datastatistics/hibernate/HibernateDataStatisticsStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datastatistics/hibernate/HibernateDataStatisticsStore.java
@@ -4,10 +4,36 @@ import org.hisp.dhis.datastatistics.DataStatistics;
 import org.hisp.dhis.datastatistics.DataStatisticsStore;
 import org.hisp.dhis.hibernate.HibernateGenericStore;
 
+import java.util.Date;
+
 /**
  * Created by yrjanaff on 19.01.2016.
  */
 public class HibernateDataStatisticsStore extends HibernateGenericStore<DataStatistics> implements DataStatisticsStore
 {
 
+    @Override public void addDataStatistics( DataStatistics dataStatistics )
+    {
+
+    }
+
+    @Override public void updateDataStatistics( DataStatistics dataStatistics )
+    {
+
+    }
+
+    @Override public void deleteDataStatistics( DataStatistics dataStatistics )
+    {
+
+    }
+
+    @Override public void getDataStatisticsInterval( Date startIterval, Date endInterval )
+    {
+
+    }
+
+    @Override public void getDataSatisticsById( int id )
+    {
+
+    }
 }
---
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datastatistics/DataStatisticsController.java
@@ -0,0 +1,12 @@
+package main.java.org.hisp.dhis.webapi.controller.datastatistics;
+
+/**
+ * Created by yrjanaff on 19.01.2016.
+ */
+
+@Controller
+//@RequestMapping( value = )
+public class DataStatisticsController
+{
+
+}
